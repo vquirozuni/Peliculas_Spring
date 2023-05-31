e9be78c1af904977ac0b795364a9f026fd51dd5f
Veiko Quiroz <v.quiroz@uniandes.edu.co>
2023-05-31 00:56:02 -0500

    first commit

---
+++ src/main/java/com/peliculas/entities/Pelicula.java
@@ -0,0 +1,72 @@
+package com.peliculas.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "peliculas")
+public class Pelicula implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	@Column(name = "fecha_estreno")
+	@Temporal(TemporalType.DATE)
+	private Date fechaEstreno;
+	private Genero genero;
+	private List<Actor> protagonistas;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFechaEstreno() {
+		return fechaEstreno;
+	}
+
+	public void setFechaEstreno(Date fechaEstreno) {
+		this.fechaEstreno = fechaEstreno;
+	}
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+
+	public List<Actor> getProtagonistas() {
+		return protagonistas;
+	}
+
+	public void setProtagonistas(List<Actor> protagonistas) {
+		this.protagonistas = protagonistas;
+	}
+
+}
